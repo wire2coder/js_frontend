0c2113bbe07a620286bab073fc39fc020c2b297c
CNNENGRING for tape bar coders <cnnicentral@turner.com>
2019-06-23 02:19:57 -0400

    re-typed some code to review

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,49 +1,55 @@
 (function () {
     const form = document.querySelector('#search-form');
-    const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
+    const searchField = document.querySelector('#search-keyword');    
     const responseContainer = document.querySelector('#response-container');
+    let searchedForText;
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
 
-        // making a new AJAX object, and setting variables
-        const imgRequest = new XMLHttpRequest();
-        imgRequest.onload = addImage;
-        imgRequest.onerror = function(err) {
-            requestError(err, 'image');
+        // make a new AJACK object
+        const imageRequest = new XMLHttpRequest();
+        imageRequest.onload = addPics; // set up a function to run
+        imageRequest.onerror = function(err) {
+            requestError(err, 'image err');
         }
 
-        imgRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
-        imgRequest.setRequestHeader('Authorization', 'Client-ID 462d22cae6dd1d4877bb082c9e9c6502893a9bb7305d4bf8f681d13b56d4abc3');
-        imgRequest.send();
+        // sending out the request
+        imageRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        imageRequest.setRequestHeader('Authorization', 'Client-ID 462d22cae6dd1d4877bb082c9e9c6502893a9bb7305d4bf8f681d13b56d4abc3');
+        imageRequest.send();
+        
 
-        // making a new AJAX object, and setting variables
-        const articleRequest = new XMLHttpRequest();
-        articleRequest.onload = addArticles;
-        articleRequest.onerror = function (err) {
-            requestError(err, 'articles');
-        };
+        // make a new AJACK object
+        const articleRequests = new XMLHttpRequest();
+        articleRequests.onload = addArticles; // set up a function to run
+        articleRequests.onerror = function(err) {
+            requestError(err, 'article err');
+        }
 
-        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=e6a9801dab184d89a4d77b94ff44048c`);
-        articleRequest.send();
+        // set up and send out request
+        articleRequests.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=e6a9801dab184d89a4d77b94ff44048c`);
+        articleRequests.send();
 
     });
 
-    function addImage() {
-        let htmlContent = '';
 
-        // the this.responseText is the imgRequest AJAX object
-        let data = JSON.parse(this.responseText);
-        
+    // add a picture to #response-container
+    /* The read-only XMLHttpRequest property responseText returns 
+        the text received from a server following a request being sent. */
+    function addPics(asdf) {
+        let asdf1 = (this.responseText); // 'this XHM object', responseText is a 'property' of an object, that's why you see the 'dot' there
+        let data = JSON.parse(asdf1); // converting text to object
+
         if ( data && data.results && data.results[0] ) {
-            let firstImage = data.results[0];
-
-            htmlContent = `
+            let firstImage = data.results[1]
+            
+            htmlContent = 
+            `
             <figure>
-                <img src="${firstImage.urls.regular}" alt="${searchedForText}" >
+            <img src="${firstImage.urls.regular}" alt="${searchedForText}" >
                 <figcaption> ${searchedForText} by ${firstImage.user.name} </figcaption>
             </figure>
             `
@@ -51,16 +57,17 @@
             htmlContent = '<div class="error-no-image">No images available</div>';
         }
 
-        // insert into HTML page
+        // insert into the HTML
         responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        
 
-    } // addImage()
+    }
 
+    // add articles to #response-container
     function addArticles() {
-        let htmlContent ='';
 
-        // this XML object
-        const data = JSON.parse(this.responseText);
+        let htmlContent ='';
+        const data = JSON.parse(this.responseText); // 'this XML object'
 
         if (data.response && data.response.docs && data.response.docs.length > 1) {
             
@@ -77,6 +84,7 @@
             htmlContent = '<div class="error-no-articles">No articles available</div>';
         }
 
+        // insert into the DOM
         responseContainer.insertAdjacentHTML('beforeend', htmlContent);
 
     }
